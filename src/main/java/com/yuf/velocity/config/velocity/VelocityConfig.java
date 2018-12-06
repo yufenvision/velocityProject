804e6cf6267dfd57e6e6abbafb82514dede4def3
yuf <duanyufeii@qq.com>
2018-12-06 18:04:48 +0800

    提交

---
+++ src/main/java/com/yuf/velocity/config/velocity/VelocityConfig.java
@@ -0,0 +1,11 @@
+package com.yuf.velocity.config.velocity;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class VelocityConfig {
+
+	public VelocityConfig() {
+		System.out.println("velocity的初始化配置......");
+	}
+}
--- src/main/java/com/yuf/velocity/test/HelloVelocity.java
+++ src/main/java/com/yuf/velocity/test/HelloVelocity.java
@@ -1,8 +1,12 @@
 package com.yuf.velocity.test;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.StringWriter;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -23,12 +27,14 @@ public class HelloVelocity {
 		ve.init();
 		
 		// 获取模板文件
-		Template t = ve.getTemplate("templates/hellovelocity.vm");
+//		Template t = ve.getTemplate("templates/hellovelocity.vm");
+		Template t = ve.getTemplate("templates/news.html");
 		// 设置变量
 		VelocityContext ctx = new VelocityContext();
 		ctx.put("name", "velocity"); 
 		ctx.put("date", (new Date()).toString()); 
-		 
+		ctx.put("username", "自动生成html文件");
+		
 		List temp = new ArrayList(); 
 		temp.add("1"); 
 		temp.add("2"); 
@@ -39,18 +45,45 @@ public class HelloVelocity {
 		t.merge(ctx, sw);
 		System.out.println(sw.toString());
 		
+		//获取服务器存储路径
+		String dirPath = "application-dev.properties";
+		Properties p = getConfigProperties(dirPath);
+		System.out.println(p.get("server.storage.path"));
+		System.out.println(p.getProperty("server.storage.path"));
+		
+		try {
+			File dir = new File(dirPath);
+			if(!dir.exists()){
+				dir.mkdirs();
+			}
+			File file = new File(dirPath + File.separator + "file.html");
+			file.createNewFile();
+			FileOutputStream out = new FileOutputStream(file);
+			out.write(sw.toString().getBytes());;
+			out.close();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
 		System.out.println("----------------");
 		
+		
+	}
+	
+	public static Properties getConfigProperties(String propertiesName){
 		Properties p = new Properties();
 		String path = Thread.currentThread().getContextClassLoader().getResource("").getPath();
 		try {
-			FileInputStream in = new FileInputStream(path+"application.properties");
+			FileInputStream in = new FileInputStream(path+propertiesName);
 			p.load(in);
 			in.close();
-			System.out.println(p);
+//			System.out.println(p);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		return p;
 	}
+	
 }
